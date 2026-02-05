192e76305569f4225bde42ebd6a480a9ce8734ab
Aritzz31 <aritznavatavo@gmail.com>
2026-02-05 12:17:41 +0100

    metodos nuevos

--- src/mainGestionBiblioteca/GestionarBiblioteca.java
+++ src/mainGestionBiblioteca/GestionarBiblioteca.java
@@ -52,7 +52,7 @@ public class GestionarBiblioteca {
 				buscarUsuarioXId(fichU);
 				break;
 			case 8:
-
+				modificarDatosLibros(fichL);
 				break;
 			case 9:
 				System.out.println("Saliendo del programa...");
@@ -118,7 +118,7 @@ public class GestionarBiblioteca {
 				moos.close();
 			}
 			System.out.println("Se ha creado el usuario '" + nuevoUsuario.getNombre()
-					+ "' correctamente. Este sera el ID del Usuario: " + nuevoUsuario.getIdUsuario());
+			+ "' correctamente. Este sera el ID del Usuario: " + nuevoUsuario.getIdUsuario());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -130,34 +130,53 @@ public class GestionarBiblioteca {
 			ObjectOutputStream oos;
 			try {
 				oos = new ObjectOutputStream(new FileOutputStream(fichL, true));
-				Libro libroF1 = new LFisico("El Quijote", "1234567890123", "Miguel de Cervantes", Genero.AVENTURAS,
-						true, 5);
+
+				Libro libroF1 = new LFisico("El Quijote", "1234567890123", "Miguel de Cervantes", Genero.AVENTURAS,true, 5);
 				Libro libroF2 = new LFisico("Dracula", "3216549876543", "Bram Stoker", Genero.TERROR, false, 3);
 				Libro libroF3 = new LFisico("Tin Tin", "9263282332482", "Georges Remi", Genero.AVENTURAS, true, 8);
 				Libro libroF4 = new LFisico("One Piece", "2637272883232", "Eiichiro Oda", Genero.AVENTURAS, false, 7);
-				Libro libroF5 = new LFisico("Orgullo y prejuicio", "8462835007841", "Jane Austen", Genero.ROMANCE, true,
-						4);
-				Libro libroD1 = new LDigital("1984", "9876543210123", "George Orwell", Genero.FICCION, Formato.PDF,
-						2.5);
-				Libro libroD2 = new LDigital("El Principito", "4567891230123", "Antoine de Saint-Exupéry",
-						Genero.AVENTURAS, Formato.TXT, 1.2);
-				Libro libroD3 = new LDigital("Fahrenheit 451", "1192820576467", "Ray Bradbury", Genero.DISTOPICO,
-						Formato.PDF, 2.5);
+				Libro libroF5 = new LFisico("Orgullo y prejuicio", "8462835007841", "Jane Austen", Genero.ROMANCE, true,4);
+				Libro libroF6 = new LFisico("Los Juegos del Hambre", "9780439023528", "Suzanne Collins", Genero.DISTOPICO, true, 6);
+				Libro libroF7 = new LFisico("El Hobbit", "9780547928227", "J.R.R. Tolkien", Genero.FICCION, true, 10);
+				Libro libroF8 = new LFisico("Sherlock Holmes", "9788491050293", "Arthur Conan Doyle", Genero.DISTOPICO, false, 4);
+				Libro libroF9 = new LFisico("La Historia Interminable", "9788420435737", "Michael Ende", Genero.FICCION, true, 5);
+				Libro libroF10 = new LFisico("Romeo y Julieta", "9788497941021", "William Shakespeare", Genero.ROMANCE, false, 2);
+
+				Libro libroD1 = new LDigital("1984", "9876543210123", "George Orwell", Genero.FICCION, Formato.PDF,2.5);
+				Libro libroD2 = new LDigital("El Principito", "4567891230123", "Antoine de Saint-Exupéry",Genero.AVENTURAS, Formato.TXT, 1.2);
+				Libro libroD3 = new LDigital("Fahrenheit 451", "1192820576467", "Ray Bradbury", Genero.DISTOPICO,Formato.PDF, 2.5);
 				Libro libroD4 = new LDigital("It", "0000000483628", "Stephen King", Genero.TERROR, Formato.EPUB, 1.2);
-				Libro libroD5 = new LDigital("Dune", "4863766767676", "Frank Herbert", Genero.FICCION, Formato.TXT,
-						2.5);
+				Libro libroD5 = new LDigital("Dune", "4863766767676", "Frank Herbert", Genero.FICCION, Formato.TXT,2.5);
+				Libro libroD6 = new LDigital("La Metamorfosis", "9788420674204", "Franz Kafka", Genero.FICCION, Formato.PDF, 1.1);
+				Libro libroD7 = new LDigital("Ready Player One", "9780307887443", "Ernest Cline", Genero.FICCION, Formato.EPUB, 3.0);
+				Libro libroD8 = new LDigital("El Código Da Vinci", "9780307474278", "Dan Brown", Genero.DISTOPICO, Formato.PDF, 2.8);
+				Libro libroD9 = new LDigital("La Sombra del Viento", "9788408172177", "Carlos Ruiz Zafón", Genero.DISTOPICO, Formato.EPUB, 2.6);
+				Libro libroD10 = new LDigital("El Alquimista", "9780061122415", "Paulo Coelho", Genero.AVENTURAS, Formato.TXT, 1.0);
 
 				oos.writeObject(libroF1);
 				oos.writeObject(libroF2);
 				oos.writeObject(libroF3);
 				oos.writeObject(libroF4);
 				oos.writeObject(libroF5);
+				oos.writeObject(libroF6);
+				oos.writeObject(libroF7);
+				oos.writeObject(libroF8);
+				oos.writeObject(libroF9);
+				oos.writeObject(libroF10);
+
 				oos.writeObject(libroD1);
 				oos.writeObject(libroD2);
 				oos.writeObject(libroD3);
 				oos.writeObject(libroD4);
 				oos.writeObject(libroD5);
+				oos.writeObject(libroD6);
+				oos.writeObject(libroD7);
+				oos.writeObject(libroD8);
+				oos.writeObject(libroD9);
+				oos.writeObject(libroD10);
+
 				oos.close();
+
 
 			} catch (IOException e) {
 
@@ -265,21 +284,17 @@ public class GestionarBiblioteca {
 				oos.close();
 				boolean borrado = fichU.delete();
 				boolean renombrado = tempFile.renameTo(fichU);
-
 				if (!borrado) {
 					System.out.println("No se pudo borrar el archivo original");
 				}
-
 				if (!renombrado) {
 					System.out.println("No se pudo renombrar el archivo temporal");
 				}
-
 				if (encontrado) {
 					System.out.println("Libro añadido al usuario correctamente");
 				} else {
 					System.out.println("Usuario no encontrado");
 				}
-
 			} catch (IOException | ClassNotFoundException e) {
 				System.out.println("Error procesando el fichero de usuarios");
 			}
@@ -340,7 +355,7 @@ public class GestionarBiblioteca {
 			oos.close();
 			fichU.delete();
 			temp.renameTo(fichU);
-			
+
 			if (!encontradoUsuario) {
 				System.out.println("Usuario no encontrado");
 			} else if (!libroEliminado) {
@@ -476,7 +491,7 @@ public class GestionarBiblioteca {
 						finArchivo = true;
 					}
 				}
-				
+
 				ois.close();
 				oos.close();
 				if (!fichU.delete()) {
@@ -496,5 +511,138 @@ public class GestionarBiblioteca {
 		} else {
 			System.out.println("El fichero no existe");
 		}
+	}
+
+	private static void modificarDatosLibros(File fichL) {
+		boolean continuar=true,encontrado = false;
+		boolean finArchivo = false;
+		boolean error = false;
+		int opcion;
+		String isbnBuscado;
+
+		if (!fichL.exists()) {
+			System.out.println("El fichero de libros no existe.");
+			continuar = false;
+		}
+		if(continuar) {
+			System.out.println("Introduce el ISBN del libro a modificar:");
+			isbnBuscado = Utilidades.introducirCadena();
+			File temp = new File("tempLibros.dat");
+			try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fichL));
+					ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(temp))) {
+
+				while(!finArchivo) {
+					try {
+						Libro libro = (Libro) ois.readObject();
+						if (!encontrado && libro.getIsbn().equals(isbnBuscado)) {
+							encontrado = true;
+
+							do {
+								System.out.println("Libro encontrado:");
+								System.out.println(libro);
+
+								opcion = submenuModificarLibro(libro);
+
+								switch (opcion) {
+								case 1:
+									System.out.println("Nuevo título:");
+									libro.setTitulo(Utilidades.introducirCadena());
+									break;
+
+								case 2:
+									System.out.println("Nuevo autor:");
+									libro.setAutor(Utilidades.introducirCadena());
+									break;
+
+								case 3:
+									System.out.println("Nuevo género:");
+									try {
+										libro.setGenero(Genero.valueOf(Utilidades.introducirCadena().toUpperCase()));
+									} catch (IllegalArgumentException e) {
+										System.out.println("Género inválido.");
+									}
+									break;
+
+									
+								case 4:
+									if (libro instanceof LFisico) {
+										LFisico lf = (LFisico) libro;
+										System.out.println("Nuevo número de disponibles:");
+										lf.setDisponibles(Utilidades.leerInt(0, 9999));
+									}
+									break;
+
+								case 5:
+									if (libro instanceof LDigital) {
+										LDigital ld = (LDigital) libro;
+										System.out.println("Nuevo formato:");
+										try {
+											ld.setFormato(Formato.valueOf(Utilidades.introducirCadena().toUpperCase()));
+										} catch (IllegalArgumentException e) {
+											System.out.println("Formato inválido.");
+										}
+									}
+									break;
+
+								case 6:
+									if (libro instanceof LDigital) {
+										LDigital ld = (LDigital) libro;
+										System.out.println("Nuevo tamaño en MB:");
+										ld.setTamanoMB(Utilidades.leerDouble());
+									}
+									break;
+
+								case 0:
+									System.out.println("Guardando cambios...");
+									break;
+								}
+
+							} while (opcion != 0);
+						}
+
+						oos.writeObject(libro);
+
+					}catch (EOFException e) {
+						finArchivo = true;
+
+					}
+				}
+			} catch (IOException | ClassNotFoundException e) {
+	            System.out.println("Error procesando el fichero de libros.");
+	            error = true;
+	        }
+
+	        if (!error) {
+	            boolean borrado = fichL.delete();
+	            boolean renombrado = temp.renameTo(fichL);
+
+	            if (!borrado) System.out.println("No se pudo borrar el fichero original.");
+	            if (!renombrado) System.out.println("No se pudo renombrar el fichero temporal.");
+
+	            if (encontrado)
+	                System.out.println("Libro modificado correctamente.");
+	            else
+	                System.out.println("No se encontró ningún libro con ese ISBN.");
+	        }
+		}
+	}
+
+	private static int submenuModificarLibro(Libro libro) {
+		System.out.println("--- MENÚ MODIFICAR LIBRO ---");
+		System.out.println("1- Cambiar título");
+		System.out.println("2- Cambiar autor");
+		System.out.println("3- Cambiar género");
+
+		if (libro instanceof LFisico) {
+			System.out.println("4- Cambiar disponibles (solo en libro físico)");
+		}
+
+		if (libro instanceof LDigital) {
+			System.out.println("5- Cambiar formato (solo en libro digital)");
+			System.out.println("6- Cambiar tamaño MB (solo en libro digital)");
+		}
+
+		System.out.println("0- Guardar y salir");
+		return Utilidades.leerInt(0, 6);
 	}
 }
